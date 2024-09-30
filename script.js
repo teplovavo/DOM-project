2e31576d8d77aba3a3062295ce212426ac5eba4c
Veronika Teplova <teplovavo@gmail.com>
2024-09-30 15:14:34 -0700

    dublicate and sort

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 console.log("LET'S START SBA 316! Veronika Teplova's WEB APPLICATION")
 
+//items
+const cardImages = [
+    '🍭', '🍬', '🍫', '🍩', '🍪',
+    '🍰', '🍦', '🍧', '🍿', '🍎'
+];
+console.log(cardImages);
+
+// Dublicate items (x2 cards)
+let cardSet = [...cardImages, ...cardImages];
+
+// Sort cards in random mode
+cardSet = cardSet.sort(() => 0.5 - Math.random());
+
+const cardGrid = document.getElementById('card-grid');
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+let matchedPairs = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
